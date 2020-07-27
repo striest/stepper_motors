f42ef4cc70d445d72f579369d202b6ed1ba8e047
striest <striest@Samuels-MacBook-Pro.local>
2020-07-27 15:52:14 -0700

    added a virtual stepper with matplotlib for remote workshops

--- stepper_motors/stepper.py
+++ stepper_motors/stepper.py
@@ -3,108 +3,130 @@ from gpiozero import OutputDevice
 from math import pi
 
 class Stepper:
-    """
-    Class for position control of stepper motors with Raspberry pi
-    Maybe velocity control later. Note that these are open-loop (should probably do a closed-loop one if I buy potentiometers)
-    """
-    
-    def __init__(self, pin1, pin2, pin3, pin4, n_steps = 512, delay = 1e-2):
-            """
-            Stepper motors use four GPIO pins.
-            n_steps: The amount of steps required for a full rotation of the stepper motor
-            delay: Need a delay to step in the step sequence (0.001 was the highest value tested)
-            """
-            self.pins = [OutputDevice(pin1), OutputDevice(pin2), OutputDevice(pin3), OutputDevice(pin4)]
-            self.n_steps = n_steps
-            self.delay = delay
-            self.step_size = 2 * pi / self.n_steps
-            
-            self.step_sequence = [
-                    [1, 0, 0, 1],
-                    [1, 0, 0, 0],
-                    [1, 1, 0, 0],
-                    [0, 1, 0, 0],
-                    [0, 1, 1, 0],
-                    [0, 0, 1, 0],
-                    [0, 0, 1, 1],
-                    [0, 0, 0, 1]
-           ]
-            self.angle = 0.0
-            self.check()
-            self.inv = False
+	"""
+	Class for position control of stepper motors with Raspberry pi
+	Maybe velocity control later. Note that these are open-loop (should probably do a closed-loop one if I buy potentiometers)
+	"""
+	
+	def __init__(self, pin1, pin2, pin3, pin4, step_mode = 'half', n_steps = 512, delay = 1e-2):
+		"""
+		Stepper motors use four GPIO pins.
 
-    def reverse(self):
-        self.inv = True
+		n_steps: The amount of steps required for a full rotation of the stepper motor
+		delay: Need a delay to step in the step sequence (0.001 was the highest value tested)
+		"""
+		assert step_mode in ['full', 'half'], 'Expected step_mode to be one of [\'full\', \'half\']. Got {}'.format(step_mode)
+		self.pins = [OutputDevice(pin1), OutputDevice(pin2), OutputDevice(pin3), OutputDevice(pin4)]
+		self.n_steps = n_steps
+		self.delay = delay
+		self.step_size = 2 * pi / self.n_steps
+		self.step_idx = 0
+		
+		if step_mode == 'half':
+			self.step_sequence = [
+				[1, 0, 0, 1],
+				[1, 0, 0, 0],
+				[1, 1, 0, 0],
+				[0, 1, 0, 0],
+				[0, 1, 1, 0],
+				[0, 0, 1, 0],
+				[0, 0, 1, 1],
+				[0, 0, 0, 1]
+			]
+			self.step_sequence_len = 8
+		elif step_mode == 'full':
+			self.step_sequence = [
+				[1, 1, 0, 0],
+				[0, 1, 1, 0],
+				[0, 0, 1, 1],
+				[1, 0, 0, 1]
+			]
+			self.step_sequence_len = 4
+		
+		self.angle = 0.0
+		self.check()
+		self.inv = False
 
-    def unreverse(self):
-        self.inv = False
+	def set_step_sequence(self, seq):
+		"""
+		seq: The step sequence to move clockwise
+		"""
+		self.step_sequence = seq
+		self.step_sequence_len = len(seq)
 
-    def rotate_to(self, angle, degrees = False):
-        """
-        Rotates to the angle specified (chooses the direction of minimum rotation)
-        """
-        target = angle * pi / 180 if degrees else angle
+	def reverse(self):
+		self.inv = True
 
-        curr = self.angle
-        diff = (target - curr) % (2*pi)
-        if abs(diff - (2*pi)) < diff:
-            diff = diff - (2*pi)
-        self.rotate_by(diff)
+	def unreverse(self):
+		self.inv = False
 
-    def rotate_by(self, angle, degrees = False):
-        """
-        Rotate the stepper by this angle (radians unless specified)
-        Positive angles rotate clockwise, negative angles rotate counterclockwise
-        """
-        target = angle * pi / 180 if degrees else angle
-        if self.inv:
-            target = -target
+	def rotate_to(self, angle, degrees = False):
+		"""
+		Rotates to the angle specified (chooses the direction of minimum rotation)
+		"""
+		target = angle * pi / 180 if degrees else angle
 
-        if target > 0:
-            n = int(target // self.step_size)
-            diff = n * self.step_size # Don't accrue error
-            for _ in range(n):
-                self.step_c()
+		curr = self.angle
+		diff = (target - curr) % (2*pi)
+		if abs(diff - (2*pi)) < diff:
+			diff = diff - (2*pi)
+		self.rotate_by(diff)
 
-        else:
-            n = int(-target // self.step_size)
-            diff = -n * self.step_size
-            for _ in range(n):
-                self.step_cc()
+	def rotate_by(self, angle, degrees = False):
+		"""
+		Rotate the stepper by this angle (radians unless specified)
+		Positive angles rotate clockwise, negative angles rotate counterclockwise
+		"""
+		target = angle * pi / 180 if degrees else angle
+		if self.inv:
+			target = -target
 
-        if self.inv:
-            diff  = -diff
-        self.angle += diff
+		if target > 0:
+			n = int(target // self.step_size)
+			diff = n * self.step_size # Don't accrue error
+			for _ in range(n):
+				self.step_c()
 
-    def zero(self):
-        """
-        Resets the position of the stepper to 0
-        """
-        self.angle = 0.0
+		else:
+			n = int(-target // self.step_size)
+			diff = -n * self.step_size
+			for _ in range(n):
+				self.step_cc()
 
-    def assign(self, assignment):
-        for i, a in enumerate(assignment):
-            if a:
-                self.pins[i].on()
-            else:
-                self.pins[i].off()
+		if self.inv:
+			diff  = -diff
 
-    def step_c(self):
-        for step in self.step_sequence:
-            self.assign(step)
-            time.sleep(self.delay)
+	def zero(self):
+		"""
+		Resets the position of the stepper to 0
+		"""
+		self.angle = 0.0
 
-    def step_cc(self):
-        for step in reversed(self.step_sequence):
-            self.assign(step)
-            time.sleep(self.delay)
+	def assign(self, assignment):
+		for i, a in enumerate(assignment):
+			if a:
+				self.pins[i].on()
+			else:
+				self.pins[i].off()
 
-    def close(self):
-        for pin in self.pins:
-            pin.close()
+	def step_c(self):
+		step = self.step_sequence[self.step_idx]
+		self.assign(step)
+		self.step_idx = (self.step_idx + 1) % self.step_sequence_len
+		self.angle += self.step_size
+		time.sleep(self.delay)
 
-    def check(self):
-        for pin in self.pins:
-            pin.on()
-            time.sleep(0.1)
-            pin.off()
+	def step_cc(self):
+		step = self.step_sequence[self.step_idx]
+		self.assign(step)
+		self.step_idx = (self.step_idx - 1) % self.step_sequence_len
+		self.angle -= self.step_size
+		time.sleep(self.delay)
+
+	def close(self):
+		for pin in self.pins:
+			pin.close()
+
+	def check(self):
+		self.step_c()
+		self.step_cc()
---
+++ stepper_motors/virtual_stepper.py
@@ -0,0 +1,101 @@
+import time
+import matplotlib.pyplot as plt
+from math import pi, sin, cos
+
+class VirtualStepper:
+	"""
+	A visualization of a stepper motor for use in remote learning workshops where not everyone has access to stepper motors.
+	"""
+	count = 0
+	
+	def __init__(self, name = None, n_steps = 1000, delay = 1e-3):
+		"""
+		Since virtual steppers are virtual, we don't need pins or step sequences. We're still using delay and n_steps to resemble physical steppers.
+		"""
+		self.fig, self.ax = plt.subplots(figsize=(3, 3))
+		self.n_steps = n_steps
+		self.delay = delay
+		self.step_size = 2 * pi / self.n_steps
+
+		if name is None:
+			self.name = 'Stepper {}'.format(VirtualStepper.count + 1)
+
+		self.angle = 0.0
+		self.check()
+		self.inv = False
+		VirtualStepper.count += 1
+
+		plt.ion()
+		plt.show()
+		self.draw()
+
+	def draw(self):
+		self.ax.cla()
+		self.ax.set_title('{} Angle = {:.2f}'.format(self.name, self.angle))
+		self.ax.set_xlim(-1, 1)
+		self.ax.set_ylim(-1, 1)
+
+		self.ax.arrow(0, 0, cos(self.angle), sin(self.angle), length_includes_head = True, head_length = 0.3, head_width = 0.1)
+
+		self.fig.canvas.draw()
+		plt.pause(1e-3)
+
+	def reverse(self):
+		self.inv = True
+
+	def unreverse(self):
+		self.inv = False
+
+	def rotate_to(self, angle, degrees = False):
+		"""
+		Rotates to the angle specified (chooses the direction of minimum rotation)
+		"""
+		target = angle * pi / 180 if degrees else angle
+
+		curr = self.angle
+		diff = (target - curr) % (2*pi)
+		if abs(diff - (2*pi)) < diff:
+			diff = diff - (2*pi)
+		self.rotate_by(diff)
+
+	def rotate_by(self, angle, degrees = False):
+		"""
+		Rotate the stepper by this angle (radians unless specified)
+		Positive angles rotate clockwise, negative angles rotate counterclockwise
+		"""
+		target = angle * pi / 180 if degrees else angle
+		if self.inv:
+			target = -target
+
+		if target > 0:
+			n = int(target // self.step_size) + 1
+			for _ in range(n):
+				self.step_c()
+
+		else:
+			n = int(-target // self.step_size) + 1
+			for _ in range(n):
+				self.step_cc()
+
+		if self.inv:
+			diff  = -diff
+
+	def zero(self):
+		"""
+		Resets the position of the stepper to 0
+		"""
+		self.angle = 0.0
+
+	def step_c(self):
+		self.angle += self.step_size
+		self.draw()
+		time.sleep(self.delay)
+
+	def step_cc(self):
+		self.angle -= self.step_size
+		self.draw()
+		time.sleep(self.delay)
+
+	def check(self):
+		self.step_c()
+		self.step_cc()
